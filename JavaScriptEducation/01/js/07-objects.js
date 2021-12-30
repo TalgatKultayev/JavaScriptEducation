b2e899b272767deffffbcfb92ff7d58cd2272a18
TalgatKultayev <87971947+TalgatKultayev@users.noreply.github.com>
2021-12-30 18:12:53 +0600

    latest commit

--- JavaScriptEducation/01/js/07-objects.js
+++ JavaScriptEducation/01/js/07-objects.js
@@ -8,11 +8,35 @@ const options = {
     colors: {
         border: "black",
         background: "red"
+    },
+    makeTest: function() { // make custom method
+        console.log('Test');
     }
 };
 
-console.log(options);
+console.log(Object.keys(options).length); // count keys == length
+options.makeTest();
 
-delete options.name;
+const {border, background} = options.colors; // destructure objects
+console.log(background);
+// console.log(options);
 
-console.log(options);
+// delete options.name;
+
+// console.log(options);
+let counter = 0;
+for (let key in options) { // use 'in' for objects. use 'of' for arrays
+    if (typeof (options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Setting ${i} in key ${key} has value of ${options[key][i]}`);
+
+        }
+        counter++;
+    } else {
+        console.log(`Setting ${key} has value of ${options[key]}`);
+        counter++;
+    }
+
+}
+console.log(counter);
+
